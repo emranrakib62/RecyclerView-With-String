7224ee63751e12286e6ac3f73f875c000fa66da9
Emranul Hoque <emranrakib01862@gmail.com>
2025-06-13 20:41:13 +0600

    updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.recyclerviewwithstring"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.recyclerviewwithstring"
@@ -32,6 +32,10 @@ android {
     }
     kotlinOptions {
         jvmTarget = "11"
+    }
+
+    buildFeatures {
+        viewBinding = true
     }
 }
 
