7eebf8676c4653c105bab15317937e9a7df77dba
Emranul Hoque <emranrakib01862@gmail.com>
2025-06-14 11:57:11 +0600

    updated

--- app/src/main/java/com/example/recyclerviewwithstring/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewwithstring/MainActivity.kt
@@ -5,12 +5,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.recyclerviewwithstring.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    var nameList= listOf<String>("Mehedi","Masum","Avijit","Mehedi","Masum","Avijit")
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
     }
 }
---
+++ app/src/main/java/com/example/recyclerviewwithstring/NameViewHolder.kt
@@ -0,0 +1,7 @@
+package com.example.recyclerviewwithstring
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerviewwithstring.databinding.ActivityMainBinding
+
+class NameViewHolder (val binding: ItemNameBinding) :RecyclerView.ViewHolder(binding.root){
+}
