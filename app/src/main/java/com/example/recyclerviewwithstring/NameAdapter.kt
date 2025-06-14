b5893c5e15cda263e1678627ee3eaf820d661ff3
Emranul Hoque <emranrakib01862@gmail.com>
2025-06-15 00:54:28 +0600

    updated

--- app/src/main/java/com/example/recyclerviewwithstring/NameAdapter.kt
+++ app/src/main/java/com/example/recyclerviewwithstring/NameAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.recyclerviewwithstring
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams
@@ -9,8 +10,9 @@ import androidx.recyclerview.widget.ListAdapter
 
 import com.example.recyclerviewwithstring.databinding.ItemNameBinding
 
-class NameAdapter : ListAdapter<String, NameViewHolder>() {
+class NameAdapter : ListAdapter<String, NameViewHolder>(comparator) {
 
+    @SuppressLint("SuspiciousIndentation")
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NameViewHolder {
       val binding=ItemNameBinding.inflate(LayoutInflater.from(parent.context),parent,false)
         return NameViewHolder(binding)
