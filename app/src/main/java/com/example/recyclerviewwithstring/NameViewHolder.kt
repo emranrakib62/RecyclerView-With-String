6c770852472bacaddab5dc6743f66fd69762e6e7
Emranul Hoque <emranrakib01862@gmail.com>
2025-06-14 12:07:08 +0600

    updated

--- app/src/main/java/com/example/recyclerviewwithstring/NameViewHolder.kt
+++ app/src/main/java/com/example/recyclerviewwithstring/NameViewHolder.kt
@@ -1,6 +1,7 @@
 package com.example.recyclerviewwithstring
 
 import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerviewwithstring.databinding.ItemNameBinding
 
 
 class NameViewHolder(val binding: ItemNameBinding) : RecyclerView.ViewHolder(binding.root){
